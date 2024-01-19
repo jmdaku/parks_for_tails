e6ee22de028f447f16719ff80f71826259c86a44
Bjorlow <dimitritorres34@gmail.com>
2024-01-19 11:38:05 -0600

    Merge remote-tracking branch 'origin/Maybe' into Maybe

--- Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/controllers/RegisterController.java
+++ Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/controllers/RegisterController.java
@@ -1,16 +1,37 @@
 package com.launchcode.parks_for_tails.controllers;
+import com.launchcode.parks_for_tails.models.User;
+
+import com.launchcode.parks_for_tails.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Controller class for handling registration-related requests.
- * The @Controller annotation marks this class as a Spring MVC controller.
- */
 @Controller
+@RequestMapping("/signup")
 public class RegisterController {
 
-    @GetMapping("/signup")
-    public String register() {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String showRegistrationForm() {
         return "register";
+    }
+
+    @PostMapping
+    public String registerUser(@RequestParam String username, @RequestParam String password) {
+        // Create a new user instance
+        User user = new User();
+        user.setUsername(username);
+        user.setRawPassword(password); // Use setRawPassword instead of setPassword
+
+        // Save the user to the database
+        userRepository.save(user);
+
+        // Redirect to a success page or login page
+        return "redirect:/login";
     }
 }
--- Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/models/User.java
+++ Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/models/User.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 import java.util.Objects;
 
-
 @Entity
 public class User {
 
@@ -29,16 +28,16 @@ public class User {
     @NotNull
     private String pwhash;
 
+    // New field to store the raw (unencoded) password temporarily
+    @Transient
+    private String rawPassword;
+
     // List of comments made by the user.
     // This establishes a one-to-many relationship with ParkComment entities.
     // The mappedBy attribute indicates the inverse side of the relationship (user field in ParkComment).
     // CascadeType.ALL ensures that any changes to a User entity cascade to its associated comments.
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<ParkComment> comments;
-
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-//    private List<ParkFavorite> favorites;
-// need to connect comments to favorites
 
     // Default constructor required by JPA
     public User() {}
@@ -47,17 +46,24 @@ public class User {
     public User(String username, String password) {
         this.id = id;
         this.username = username;
-        this.pwhash = encoder.encode(password);
+        this.rawPassword = password;
+        this.pwhash = encoder.encode(rawPassword);
     }
 
     // Encoder instance for encrypting and verifying passwords
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    // Setter for the rawPassword field
+    public void setRawPassword(String rawPassword) {
+        this.rawPassword = rawPassword;
+        // Encrypt the raw password and set it to the pwhash field
+        this.pwhash = encoder.encode(rawPassword);
+    }
+
     //Check if the given password matches the stored encrypted password
     public boolean isMatchingPassword(String password) {
         return encoder.matches(password, pwhash);
     }
-
 
     // Getters & Setters:
     public int getId() {
@@ -84,9 +90,6 @@ public class User {
         return comments;
     }
 
-
-
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -95,8 +98,6 @@ public class User {
         if (id == user.id) if (Objects.equals(username, user.username)) return true;
         return false;
     }
-
-    // may only need to match id?
 
     @Override
     public int hashCode() {
