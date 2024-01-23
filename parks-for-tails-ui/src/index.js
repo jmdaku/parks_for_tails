daf6da295bf663e61e0a02ad4be20415668cb67b
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-23 12:35:49 -0600

    Update index.js

--- parks-for-tails-ui/src/index.js
+++ parks-for-tails-ui/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root') // Ensure this is correct
 );
 
 // If you want to start measuring performance in your app, pass a function
