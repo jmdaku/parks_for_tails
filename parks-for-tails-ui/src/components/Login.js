006686cde18f40d8da87d8125a75452808b945b6
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-24 21:44:52 -0600

    Update Login.js

--- parks-for-tails-ui/src/components/Login.js
+++ parks-for-tails-ui/src/components/Login.js
@@ -1,91 +1,83 @@
-import React from 'react';
-import parksfortailssm from './parksfortailssm.png';
-import {  useState, useContext } from "react";
-import { Link, useNavigate } from 'react-router-dom';
-import { LoginContext } from './checkLogin.jsx';
+import React, { useState } from 'react';
+import { MDBContainer, MDBInput, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import {
-  MDBContainer,
-  MDBInput,
-  MDBBtn,
-  }
-from 'mdb-react-ui-kit';
 
-export default function Login() {
-
-  const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [verifyPassword, setVerifyPassword] = useState('');
-
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    // if (password !== verifyPassword) {
-    //   alert("Passwords do not match");
-    //   return;
-    // }
 
     try {
-      const response = await axios.post('http://localhost:8080/api/users/login', {
-        username,
-        password,
-      });
-
-      console.log('Response Body:', response.data);
-
-      if (response.data.success) {
-        localStorage.setItem("userId", response.data.userId);
-        navigate('/home');
-        setIsLoggedIn(true);
-
-         // Redirect to the profile page
+      const response = await axios.post('http://localhost:8080/api/users/login', { username, password });
+  
+      if (response.status === 200) {
+        // Login successful
         navigate('/profile');
       } else {
-        alert(response.data.message);
+        // Login failed
+        alert('Login failed. Please check your credentials.');
       }
     } catch (error) {
-      console.error('Error:', error);
-
+      console.error('Error during login:', error);
       alert('An error occurred during login. Please try again.');
     }
   };
-
   return (
-    <div className='App'>
-      <img src={parksfortailssm} alt="logos" width={1000} height={300} />
-      
-      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-        <form onSubmit={handleLogin}>
-          <MDBInput
-            wrapperClass='mb-4'
-            label='Username'
-            size='lg'
-            id='form1'
-            type='text'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-  
-          <MDBInput
-            wrapperClass='mb-4'
-            label='Password'
-            size='lg'
-            id='form3'
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-  
-          <MDBBtn type="submit" className="mb-4">Sign in</MDBBtn>
-        </form>
-  
-        <div className="text-center">
-          Not a member? <Link to="/register">Register here</Link>
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <form onSubmit={handleLogin}>
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Username"
+          id="form1"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="form2"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <MDBBtn className="mb-4" size="lg" type="submit">
+          Sign in
+        </MDBBtn>
+      </form>
+
+      <div className="text-center">
+        <p>
+          Not a member? <a href="/register">Register</a>
+        </p>
+        <p>or sign up with:</p>
+
+        <div className="d-flex justify-content-between mx-auto" style={{ width: '40%' }}>
+          {/* Social media login buttons */}
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="facebook-f" size="sm" />
+          </MDBBtn>
+
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="twitter" size="sm" />
+          </MDBBtn>
+
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="google" size="sm" />
+          </MDBBtn>
+
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="github" size="sm" />
+          </MDBBtn>
         </div>
-      </MDBContainer>
-    </div>
+      </div>
+    </MDBContainer>
   );
-  
-}
+};
+
+export default Login;
