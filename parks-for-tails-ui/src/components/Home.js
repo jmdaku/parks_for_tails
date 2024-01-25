38f482fc55588ccf2b69be47ee59f0762280ae3e
jmdaku <jmdaku@gmail.com>
2024-01-24 22:55:19 -0500

    Update Home.js

--- parks-for-tails-ui/src/components/Home.js
+++ parks-for-tails-ui/src/components/Home.js
@@ -2,18 +2,23 @@ import '../App.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import parksfortailssm from './parksfortailssm.png';
-import Login from './Login.js';
-import Register from './Register.js';
+
 
 export default function Home() {
   return (
     <div className='App'>
       <img src={parksfortailssm} alt="logo" width={1400} height={500} />
       <div>
+        <p>
         <Link to="/login">Login</Link>
+        </p>
         <p>
           Not a member? <Link to="/register">Register here</Link>
         </p>
+        <p>
+          Need to add a park? <Link to="/addpark">Add a park</Link>
+        </p>
+
       </div>
     </div>
   );
