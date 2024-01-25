ce659ea1fc06679911b2f603802e79cd5b0242bb
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-25 16:42:07 -0600

    Update index.js

--- parks-for-tails-ui/src/index.js
+++ parks-for-tails-ui/src/index.js
@@ -3,33 +3,22 @@ import { createRoot } from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Profile from './components/Profile.jsx';
-import AddPark from './components/AddPark.jsx';
 import { LoginProvider } from './components/checkLogin';
 
-const container = document.getElementById('root'); 
+const container = document.getElementById('root'); // Use 'root' as the container ID
 const root = createRoot(container);
 
+
 root.render(
-  <LoginProvider>
+  <LoginProvider> {/* Wrap whole application with LoginProvider */}
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/addpark" element={<AddPark />} />
+        <Route path="*" element={<App />} />
+        {/* Other routes */}
       </Routes>
     </Router>
-  </LoginProvider>
+  </LoginProvider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
