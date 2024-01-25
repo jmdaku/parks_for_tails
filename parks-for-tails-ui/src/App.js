4b95c62e6b1e51e689eb3293da97cb96bc38a8e8
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-24 22:01:05 -0600

    Update App.js

--- parks-for-tails-ui/src/App.js
+++ parks-for-tails-ui/src/App.js
@@ -1,58 +1,24 @@
-import './App.css';
 import React from 'react';
-import { useState, useEffect } from 'react'
-
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Login } from './components/Login.js';
-// import { Register } from './components/Register.js';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import Profile from './components/Profile.js';
 
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('userId') !== null);  
-  //const [courses, setCourses] = useState([]);
-  
-  //Absolutely not sure if this is the best or most efficient way to do this, but it's what I've got figured out for now! -Sprinkles
-  const updateApp = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/home`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-    
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    
-        const data = await response.json();
-        //setCourses(data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-
+const App = () => {
   return (
-    <Router>
+    <div>
+      <h1>Welcome to My App</h1>
+      {/* Other components or layout elements could be here */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* Additional routes can be added as needed */}
       </Routes>
-    </Router>
+    </div>
   );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-// const rootContainer = document.getElementById('root');
-// const root = ReactDOM.createRoot(rootContainer);
-// root.render(<App />);
+};
 
 export default App;
-
-
