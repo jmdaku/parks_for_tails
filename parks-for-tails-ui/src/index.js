d24909c23b8d7637cb78b1d712186b83febfb68d
jmdaku <jmdaku@gmail.com>
2024-01-25 15:38:19 -0500

    Update index.js

--- parks-for-tails-ui/src/index.js
+++ parks-for-tails-ui/src/index.js
@@ -3,27 +3,31 @@ import { createRoot } from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Profile from './components/Profile.jsx';
+import AddPark from './components/AddPark.jsx';
 import { LoginProvider } from './components/checkLogin';
 
-const container = document.getElementById('root'); // Use 'root' as the container ID
+const container = document.getElementById('root'); 
 const root = createRoot(container);
 
-
 root.render(
-  <LoginProvider> {/* Wrap the entire application with LoginProvider */}
+  <LoginProvider>
     <Router>
       <Routes>
-        <Route path="*" element={<App />} />
-        {/* Other routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/addpark" element={<AddPark />} />
       </Routes>
     </Router>
-  </LoginProvider>,
+  </LoginProvider>
 );
-// Render the App component separately if needed
-// root.render(<App />, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
