8fee890f0ae67b9f02e60636a0235638ca072845
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-25 14:35:26 -0600

    Update App.js

--- parks-for-tails-ui/src/App.js
+++ parks-for-tails-ui/src/App.js
@@ -3,16 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import Profile from './components/Profile.js';
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to My App</h1>
+  
       {/* Other components or layout elements could be here */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
         {/* Additional routes can be added as needed */}
       </Routes>
     </div>
