7c94784c4300d975f1e1edac79a71f49da334b69
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-25 14:51:11 -0600

    Update login.js

--- parks-for-tails-ui/src/components/login.js
+++ parks-for-tails-ui/src/components/login.js
@@ -1,47 +1,83 @@
-import React from 'react';
-import Register from './Register';
+import React, { useState } from 'react';
+import { MDBContainer, MDBInput, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import {
-  MDBContainer,
-  MDBInput,
-  MDBBtn,
-  MDBIcon
-}
-from 'mdb-react-ui-kit';
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-export default function Login () {
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/users/login', { username, password });
+  
+      if (response.status === 200) {
+        // Login successful
+        navigate('/profile');
+      } else {
+        // Login failed
+        alert('Login failed. Please check your credentials.');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      alert('An error occurred during login. Please try again.');
+    }
+  };
   return (
     <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <form onSubmit={handleLogin}>
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Username"
+          id="form1"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="form2"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-      <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
-      <MDBBtn className="mb-4">Sign in</MDBBtn>
+        <MDBBtn className="mb-4" size="lg" type="submit">
+          Sign in
+        </MDBBtn>
+      </form>
 
       <div className="text-center">
-        <p>Not a member? <a href=".">Register</a></p>
+        <p>
+          Not a member? <a href="/register">Register</a>
+        </p>
         <p>or sign up with:</p>
 
-        <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='facebook-f' size="sm"/>
+        <div className="d-flex justify-content-between mx-auto" style={{ width: '40%' }}>
+          {/* Social media login buttons */}
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="facebook-f" size="sm" />
           </MDBBtn>
 
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='twitter' size="sm"/>
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="twitter" size="sm" />
           </MDBBtn>
 
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='google' size="sm"/>
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="google" size="sm" />
           </MDBBtn>
 
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='github' size="sm"/>
+          <MDBBtn tag="a" color="none" className="m-1" style={{ color: '#1266f1' }}>
+            <MDBIcon fab icon="github" size="sm" />
           </MDBBtn>
-
         </div>
       </div>
-
     </MDBContainer>
   );
-}
+};
+
+export default Login;
