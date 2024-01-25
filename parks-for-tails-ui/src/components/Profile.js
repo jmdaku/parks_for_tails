708e21d84dcc17386ee2e26c79f8441ae4e0bd78
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-25 16:39:01 -0600

    Update Profile.js

--- parks-for-tails-ui/src/components/Profile.js
+++ parks-for-tails-ui/src/components/Profile.js
@@ -2,37 +2,34 @@ import React, { useState, useEffect, useContext } from 'react';
 import parksfortailssm from './parksfortailssm.png';
 import { LoginContext } from './checkLogin.jsx';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 
 
+
 export default function Profile() {
+  // State variables
     const [loading, setLoading] = useState(true);
     const [editingPassword, setEditingPassword] = useState(false);
     const [userToDelete, setUserToDelete] = useState(null);
     const user = parseInt(localStorage.getItem('userId'));
     const {isLoggedIn, setIsLoggedIn} = useContext(LoginContext);
     const navigate = useNavigate();
-
     const data = {'userId': user};
-  
-
+    
     return (
-        <div className="gradient-custom-2" style={{ backgroundColor: '#9de2ff' }}>
+        <div className="gradient-custom-2" style={{ backgroundColor: '#9DE2FF' }}>
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center align-items-center h-100">
           <MDBCol lg="9" xl="7">
             <MDBCard>
               <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                                 
                 </div>
                 <div className="ms-3" style={{ marginTop: '130px' }}>
                   <MDBTypography tag="h1">Username</MDBTypography>
                 </div>
               </div>
-              <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
-                
+              <div className="p-4 text-black" style={{ backgroundColor: '#F8F9FA' }}>
               </div>
               <MDBCardBody className="text-black p-4">
                                 <div className="d-flex justify-content-between align-items-center mb-4">
