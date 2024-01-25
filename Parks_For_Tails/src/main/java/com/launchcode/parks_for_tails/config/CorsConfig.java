4c54a34748ea5e55c5c04ecb528c625737f7d3d3
Bjorlow <136783825+Bjorlow@users.noreply.github.com>
2024-01-25 16:56:01 -0600

    Update CorsConfig.java

--- Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/config/CorsConfig.java
+++ Parks_For_Tails/src/main/java/com/launchcode/parks_for_tails/config/CorsConfig.java
@@ -4,16 +4,19 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Configuration class for CORS (Cross-Origin Resource Sharing) settings
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+    // Configures allowed origins, methods, headers, and credentials for CORS.
     @Override
+    // specify which endpoints are allowed to be accessed from different origins.
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*") // Add this line
-                .exposedHeaders("Authorization") // Add this line if needed
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
 }
